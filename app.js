4bc0bb02351bafb53fdd3e3c6ded914b868cd45a
steff2401 <stephank@hotmail.no>
2023-07-26 20:54:55 +0200

    Refactor code to use classes

--- app.js
+++ app.js
@@ -1,122 +1,127 @@
 const divBooks = document.querySelector(".books");
 const addBookButton = document.querySelector(".add-book > button");
-const myLibrary = [];
 
-function Book(title, author, pages, read) {
+class Library {
 
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+    constructor() {
 
-function addBookToLibrary(book) {
-
-    myLibrary.push(book);
-}
-
-function changeReadStatus(book, readButton) {
-
-    if (book.read) {
-        readButton.textContent = "Not read";
-        book.read = false;
-
-    } else {
-        readButton.textContent = "Read";
-        book.read = true;
+        this.arrayOfBooks = [];
     }
-}
 
-function createBookCard(book) {
+    addBook(book) {
 
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
+        this.arrayOfBooks.push(book);
+    }
 
-    for (const property in book) {
+    deleteBook(book) {
 
-        if (property === "read") {
-
-            const readDiv = document.createElement("div");
-            readDiv.classList.add("read-div")
-            const readButton = document.createElement("button");
-
-            const readLabel = document.createElement("p");
-            readLabel.textContent = "STATUS: "
-            readDiv.appendChild(readLabel);
-
-            if (book[property]) { // true if read, else false
-                readButton.textContent = "Read";
-
-            } else {
-                readButton.textContent = "Not read";
-            }
-
-            readDiv.appendChild(readButton);
-            bookCard.appendChild(readDiv);
-            readButton.addEventListener("click", () => changeReadStatus(book, readButton));
-            continue;
-
-        } else {
-
-            const bookInfo = document.createElement("p");
-            bookInfo.textContent = property.toUpperCase() + ": " + book[property];
-            bookCard.appendChild(bookInfo);
+        const index = this.arrayOfBooks.indexOf(book);
+        if (index !== -1) {
+            this.arrayOfBooks.splice(index, 1);
         }
     }
 
-    // add delete button to each book-card
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    bookCard.appendChild(deleteButton);
-    deleteButton.addEventListener("click", () => deleteBook(book, bookCard, myLibrary));
+    displayAllBooks() {
 
-    return bookCard;
+        this.arrayOfBooks.forEach((book) => divBooks.appendChild(book.createBookCard()));
+    }
+
+    displayNewBook(newBook) {
+
+        divBooks.appendChild(newBook.createBookCard());
+    }
 }
 
-function displayAllBooks(arrayOfBooks) {
+class Book {
 
-    arrayOfBooks.forEach(book => divBooks.appendChild(createBookCard(book)));
+    constructor(title, author, pages, read) {
+
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    changeReadStatus(readButton) {
+
+        if (this.read) {
+            readButton.textContent = "Not read";
+            this.read = false;
+
+        } else {
+            readButton.textContent = "Read";
+            this.read = true;
+        }
+    }
+
+    createBookCard() {
+
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        for (const property in this) {
+            if (property === "read") {
+
+                const readDiv = document.createElement("div");
+                readDiv.classList.add("read-div");
+                const readButton = document.createElement("button");
+
+                const readLabel = document.createElement("p");
+                readLabel.textContent = "STATUS: ";
+                readDiv.appendChild(readLabel);
+
+                if (this[property]) {
+                    readButton.textContent = "Read";
+
+                } else {
+                    readButton.textContent = "Not read";
+                }
+
+                readDiv.appendChild(readButton);
+                bookCard.appendChild(readDiv);
+                readButton.addEventListener("click", () => this.changeReadStatus(readButton));
+                continue;
+
+            } else {
+                
+                const bookInfo = document.createElement("p");
+                bookInfo.textContent = property.toUpperCase() + ": " + this[property];
+                bookCard.appendChild(bookInfo);
+            }
+        }
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        bookCard.appendChild(deleteButton);
+        deleteButton.addEventListener("click", () => {
+            myLibrary.deleteBook(this);
+            bookCard.remove();
+        });
+
+        return bookCard;
+    }
 }
 
-function displayNewBook(newBook) {
-
-    divBooks.appendChild(createBookCard(newBook));
-}
-
-function deleteBook(book, bookCard, arrayOfBooks) {
-    // delete from HTML
-    bookCard.remove(); 
-
-    // delete from array
-    arrayOfBooks.splice(arrayOfBooks.indexOf(book), 1);
-
-    // delete object
-    delete book;
-}
-
-// Adding new books
-
-const form = document.createElement("form");
+const myLibrary = new Library();
 
 addBookButton.addEventListener("click", () => {
-
     document.querySelector("body").appendChild(form);
 });
 
-form.innerHTML = 
-    `
-    <input type="text" id="title" placeholder="Title" required>
-    <input type="text" id="author" placeholder="Author" required>
-    <input type="number" id="pages" placeholder="Pages" required>
-    <div>
-        <label for="read">Read?</label> 
-        <input type="checkbox" id="read">
-    </div>
-    <button id="submit-button" type="submit">Submit</button>
-    `;
+const form = document.createElement("form");
+
+form.innerHTML = `
+  <input type="text" id="title" placeholder="Title" required>
+  <input type="text" id="author" placeholder="Author" required>
+  <input type="number" id="pages" placeholder="Pages" required>
+  <div>
+      <label for="read">Read?</label> 
+      <input type="checkbox" id="read">
+  </div>
+  <button id="submit-button" type="submit">Submit</button>
+`;
 
 form.addEventListener("submit", (e) => {
-
     e.preventDefault();
 
     const title = form.querySelector("#title").value;
@@ -125,9 +130,9 @@ form.addEventListener("submit", (e) => {
     const read = form.querySelector("#read").checked;
 
     const newBook = new Book(title, author, pages, read);
-    addBookToLibrary(newBook);
-    displayNewBook(newBook);
-
-    // remove form from page after submitting
+    myLibrary.addBook(newBook);
+    myLibrary.displayNewBook(newBook);
     form.remove();
 });
+
+myLibrary.displayAllBooks();
